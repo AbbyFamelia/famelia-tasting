676f41d9f41781d5d9606ab66c321223c0d1d51c
AbbyFamelia <abby@famelia.com.au>
2025-10-27 14:26:59 +1100

    Update route.js

--- app/proxy/test/route.js
+++ app/proxy/test/route.js
@@ -1,3 +1,4 @@
+export const runtime = 'nodejs';
 export async function GET() {
   return new Response(JSON.stringify({ ok: true, where: "proxy/test" }), {
     headers: { "Content-Type": "application/json" }
